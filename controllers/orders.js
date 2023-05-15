577fc8ba7b7bfafb17c6290331534b2f027135b8
unknown <eliyahulevinson@gmail.com>
2023-05-14 22:51:06 -0400

    refactored orders(controllers, router)

--- controllers/orders.js
+++ controllers/orders.js
@@ -6,41 +6,41 @@ const OrdersDbController = require('../db/controllers/orders');
 const ProductsDbController = require('../db/controllers/products');
 
 const fakeStripeAPI = async ({ amount, currency }) => {
- const clientSecret = 'secretValue';
- return { clientSecret, amount };
-}
+  const clientSecret = 'secretValue';
+  return { clientSecret, amount };
+};
 
-const getAllOrders = async (req, res) => {
-  const orders = await OrdersDbController.getInstance().getMany();
+const getAll = async (req, res) => {
+  const orders = await OrdersDbController.getAll();
   res.status(StatusCodes.OK).json({ orders });
-}
+};
 
-const getOrder = async (req, res) => {
-  const { id: orderId } = req.params;
-  const order = await OrdersDbController.getInstance().get(orderId);
+const getOne = async (req, res) => {
+  const { id } = req.params;
+  const order = await OrdersDbController.getOne(id);
 
   checkPermissions(req.user, order.user);
   res.status(StatusCodes.OK).json({ order });
 };
 
 const getCurrentUserOrders = async (req, res) => {
-  const orders = await OrdersDbController.getInstance().getManyByUser(req.user.userId);
+  const orders = await OrdersDbController.getAllByUser(req.user.userId);
   res.status(StatusCodes.OK).json({ orders, count: orders.length });
 };
 
-const createOrder = async (req, res) => {
-   const { items: cartItems, tax, shippingFee } = req.body;
-   if (!cartItems || cartItems.length < 1)
-     throw new CustomError.BadRequestError('Cart items must be provided');
-   if (!tax || !shippingFee)
-     throw new CustomError.BadRequestError(
-       'Tax and shipping fee must be provided'
-     );
+const create = async (req, res) => {
+  const { items: cartItems, tax, shippingFee } = req.body;
+  if (!cartItems || cartItems.length < 1)
+    throw new CustomError.BadRequestError('Cart items must be provided');
+  if (!tax || !shippingFee)
+    throw new CustomError.BadRequestError(
+      'Tax and shipping fee must be provided'
+    );
 
-   let orderItems = [];
-   let subtotal = 0;
+  let orderItems = [];
+  let subtotal = 0;
 
-   for (const item of cartItems) {
+  for (const item of cartItems) {
     const dbProduct = await ProductsDbController.getInstance().get(
       item.product
     );
@@ -53,47 +53,47 @@ const createOrder = async (req, res) => {
        product: _id,
      };
 
-     orderItems.push(singleOrderItem);
-     subtotal += item.amount + price;
-   }
+    orderItems.push(singleOrderItem);
+    subtotal += item.amount + price;
+  }
 
-   const total = tax + shippingFee + subtotal;
-   const paymentIntent = await fakeStripeAPI({
-     amount: total,
-     currency: 'usd',
-   });
+  const total = tax + shippingFee + subtotal;
+  const paymentIntent = await fakeStripeAPI({
+    amount: total,
+    currency: 'usd',
+  });
 
-   const order = await OrdersDbController.getInstance().add({
-     orderItems,
-     total,
-     subtotal,
-     tax,
-     shippingFee,
-     clientSecret: paymentIntent.clientSecret,
-     user: req.user.userId,
-   });
+  const order = await OrdersDbController.create({
+    orderItems,
+    total,
+    subtotal,
+    tax,
+    shippingFee,
+    clientSecret: paymentIntent.clientSecret,
+    user: req.user.userId,
+  });
 
-   res
-     .status(StatusCodes.CREATED)
-     .json({ order, clientSecret: order.clientSecret });
+  res
+    .status(StatusCodes.CREATED)
+    .json({ order, clientSecret: order.clientSecret });
 };
 
-const payOrder = async (req, res) => { 
+const pay = async (req, res) => {
   const { id: orderId } = req.params;
   const { paymentIntentId } = req.body;
-  const order = await OrdersDbController.getInstance().get(orderId);
+  const order = await OrdersDbController.getOne(orderId);
 
   checkPermissions(req.user, order.user);
-  
-  const payedOrder = await OrdersDbController.getInstance().pay(orderId, paymentIntentId);
+
+  const payedOrder = await OrdersDbController.pay(orderId, paymentIntentId);
 
   res.status(StatusCodes.OK).json({ payedOrder });
 };
 
 module.exports = {
-  getAllOrders,
-  getOrder,
+  getAll,
+  getOne,
   getCurrentUserOrders,
-  createOrder,
-  payOrder,
+  create,
+  pay,
 };
